4a0f691a076f7e2046fdd45bed81eb2d3cbd8ca5
Tunguyen09CNTT03 <118881853+Tunguyen09CNTT03@users.noreply.github.com>
2023-04-16 13:14:46 +0700

    Add files via upload

---
+++ pages/footer.php
@@ -0,0 +1,10 @@
+<div class="clear"></div>
+		<div class="footer">
+		<h5><p class="footer_copyright">Copyright by Nguyễn Ngọc Tú 0950080079 09CNPM3 <br>
+			<a href="https://www.facebook.com/critimal.zenx">facebook liên hệ</a><br>
+			Số điện thoại liên hệ: 0356137066
+			<p></p>
+			</p>
+		</div>
+		
+
